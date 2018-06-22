0c6e950c460bf79706a2bd35de092d300f75f60a
qetennyson <quincytennyson@yahoo.com>
2018-06-22 09:41:58 -0400

    button collapse working for all cards

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,14 @@ from app import app
 def index():
     languages = [
         {
+            'id': 1,
             'lang_name': 'Python',
             'difficulty': '2',
             'common_uses': ['Data Science', 'Finance', 'Web Applications'],
             'image': 'app/static/images/python.png'
         },
         {
+            'id': 2,
             'lang_name': 'JavaScript',
             'difficulty': '2',
             'common_uses': ['Web Applications'],
@@ -19,24 +21,28 @@ def index():
 
         },
         {
+            'id': 3,
             'lang_name': 'Java',
             'difficulty': '4',
             'common_uses': ['Enterprise Applications', 'Web Applications'],
             'image': 'app/static/images/java.jpg'
         },
         {
+            'id': 4,
             'lang_name': 'Swift',
             'difficulty': '3',
             'common_uses': ['Mobile Applications'],
             'image': 'app/static/images/swift.png'
         },
         {
+            'id': 5,
             'lang_name': 'ReactJS',
             'difficulty': '3',
             'common_uses': ['Mobile Applications', 'Web Applications'],
             'image': 'app/static/images/react.png'
         },
         {
+            'id': 6,
             'lang_name': 'Android',
             'difficulty': '4',
             'common_uses': ['Mobile Applications'],
