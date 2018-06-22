5de81bf5b53f05dd8f9e6c18710206bdc9e12627
qetennyson <quincytennyson@yahoo.com>
2018-06-22 11:20:26 -0400

    added robust language data (langDict) and added to card dropdowns

---
+++ app/langDict.py
@@ -0,0 +1,62 @@
+java = {
+    'difficulty': "Java is a strongly typed object oriented language.  This makes the language "
+                  "more difficult to learn as the code tends to be more verbose.",
+    'requirements': "Java works on all modern computers.  The Java Development Kit (JDK) and popular "
+                    "Integrated Development Environments are free.  Popular IDEs include: Intelij and Eclipse.",
+    'certifications': "Java Junior Associate",
+    'libraries': "Java includes many standard libraries",
+    'pop_apps': "Blu-Ray Xlets, Android Apps",
+
+}
+
+python = {
+    'difficulty': "Python is a dynamically typed programming language.  It uses whitespace instead of braces to denote "
+                  "blocks of code.  In python there are numerous libraries that make it possible to complete complex "
+                  "tasks in relatively few lines of code.",
+    'requirements': "Python works on all modern computers.  Some python libraries may have special requirements.",
+    'certifications': "MTA Intro to Programming in Python",
+    'libraries': "Numpy, Flask, Django, Requests",
+    'pop_apps': "YouTube, Reddit, Spotify",
+
+}
+
+android = {
+    'difficulty': "Android is developed in Java or Kotlin.  Its complexity has grown as the platform has matured.  "
+                  "Many additional concepts must be remembered in addition to Java/Kotlin.",
+    'requirements': "A fast, modern computer is required for Android development because of the Android emulator.  "
+                    "Windows machines will need an Intel processor with visualization support and Linux machines will "
+                    "need an AMD processor with visualization support.  If the computer's CPU does not support "
+                    "visualization then Android development will be very slow and painful.  "
+                    "Sometimes BIOS/EFI settings need to be adjusted to enable visualization support.",
+    'certifications': "Google certifications.",
+    'libraries': "Introductory Android development will not use any libraries beyond what is provided in the SDK, "
+                 "as the program becomes more advanced the libraries such as Android Annotations "
+                 "(http://androidannotations.org/) will become useful.",
+    'pop_apps': "All Android apps",
+}
+
+javascript = {
+    'difficulty': "JavaScript is well documented and easy to learn.  The language has a number of unique "
+                  "implementations of common features that new programmers need to be aware of.",
+    'requirements': "No special requirements",
+    'certifications': "No meaningful certifications.",
+    'libraries': "JQuery, Vue.js and Node.js",
+    'pop_apps': "Most modern sites use JS in some form."
+}
+
+swift = {
+    'difficulty': "The Apple website claims that using Playgrounds to learn Swift make it 'incredibly simple and fun'",
+    'requirements': "Mac computer with an Intel i5 or i7 processor.",
+    'certifications': "No meaningful certifications.",
+    'libraries': "Cocoa and CocoaTouch",
+    'pop_apps': "Candy Crush, Facebook Messenger"
+}
+
+react = {
+    'difficulty': "ReactJS is used to create clients side web applications.  "
+                  "One will need to know JavaScript and HTML/CSS.",
+    'requirements': "No special requirements",
+    'certifications': "No meaningful certifications",
+    'libraries': "React Native",
+    'pop_apps': "AirBNB, Dropbox, Cloudflare",
+}
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
+from app import langDict
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
@@ -10,14 +12,24 @@ def index():
             'lang_name': 'Python',
             'difficulty': '2',
             'common_uses': ['Data Science', 'Finance', 'Web Applications'],
-            'image': 'app/static/images/python.png'
+            'image': 'app/static/images/python.png',
+            'diff_expanded': langDict.python['difficulty'],
+            'requirements': langDict.python['requirements'],
+            'certifications': langDict.python['certifications'],
+            'libraries': langDict.python['libraries'],
+            'pop_apps': langDict.python['pop_apps'],
         },
         {
             'id': 2,
             'lang_name': 'JavaScript',
             'difficulty': '2',
             'common_uses': ['Web Applications'],
-            'image': 'app/static/images/javascript.png'
+            'image': 'app/static/images/javascript.png',
+            'diff_expanded': langDict.javascript['difficulty'],
+            'requirements': langDict.javascript['requirements'],
+            'certifications': langDict.javascript['certifications'],
+            'libraries': langDict.javascript['libraries'],
+            'pop_apps': langDict.javascript['pop_apps'],
 
         },
         {
@@ -25,29 +37,48 @@ def index():
             'lang_name': 'Java',
             'difficulty': '4',
             'common_uses': ['Enterprise Applications', 'Web Applications'],
-            'image': 'app/static/images/java.jpg'
+            'image': 'app/static/images/java.jpg',
+            'diff_expanded': langDict.java['difficulty'],
+            'requirements': langDict.java['requirements'],
+            'certifications': langDict.java['certifications'],
+            'libraries': langDict.java['libraries'],
+            'pop_apps': langDict.java['pop_apps'],
         },
         {
             'id': 4,
             'lang_name': 'Swift',
             'difficulty': '3',
             'common_uses': ['Mobile Applications'],
-            'image': 'app/static/images/swift.png'
+            'image': 'app/static/images/swift.png',
+            'diff_expanded': langDict.swift['difficulty'],
+            'requirements': langDict.swift['requirements'],
+            'certifications': langDict.swift['certifications'],
+            'libraries': langDict.swift['libraries'],
+            'pop_apps': langDict.java['pop_apps'],
         },
         {
             'id': 5,
             'lang_name': 'ReactJS',
             'difficulty': '3',
             'common_uses': ['Mobile Applications', 'Web Applications'],
-            'image': 'app/static/images/react.png'
+            'image': 'app/static/images/react.png',
+            'diff_expanded': langDict.react['difficulty'],
+            'requirements': langDict.react['requirements'],
+            'certifications': langDict.react['certifications'],
+            'libraries': langDict.react['libraries'],
+            'pop_apps': langDict.react['pop_apps'],
         },
         {
             'id': 6,
             'lang_name': 'Android',
             'difficulty': '4',
             'common_uses': ['Mobile Applications'],
-            'image': 'app/static/images/android.png'
+            'image': 'app/static/images/android.png',
+            'diff_expanded': langDict.android['difficulty'],
+            'requirements': langDict.android['requirements'],
+            'certifications': langDict.android['certifications'],
+            'libraries': langDict.android['libraries'],
+            'pop_apps': langDict.android['pop_apps'],
         }
     ]
     return render_template('index.html', title='Language Quick Reference Guide', languages=languages)
-
